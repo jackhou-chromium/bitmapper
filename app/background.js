ff62d7575cf739e5f9b6fd84fb3d0d16f61fda10
Davina Adisusila <dadisusila@google.com>
2014-01-07 16:02:25 +1100

    Fixed minimum window size for app.
    Currently 800 x 600.
    
    R=calamity@chromium.org
    
    Review URL: https://codereview.appspot.com/48070044

--- app/background.js
+++ app/background.js
@@ -8,6 +8,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
     'width': 800,
     'height': 600,
+    'minWidth': 800,
+    'minHeight': 600,
     'type': 'shell',
   });
 });
